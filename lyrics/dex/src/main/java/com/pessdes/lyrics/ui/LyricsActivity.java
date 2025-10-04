b9a922e14a6f88a8df897097585a54bad2337513
Hazzz895 <kornilovily4@gmail.com>
2025-10-04 10:14:32 +0400

    log

--- lyrics/dex/src/main/java/com/pessdes/lyrics/ui/LyricsActivity.java
+++ lyrics/dex/src/main/java/com/pessdes/lyrics/ui/LyricsActivity.java
@@ -23,6 +23,7 @@ import com.pessdes.lyrics.components.lrclib.dto.Lyrics;
 import com.pessdes.lyrics.components.lrclib.dto.SyncedLyricsLine;
 import com.pessdes.lyrics.ui.components.cells.LyricsCell;
 import com.pessdes.lyrics.ui.components.cells.PlainLyricsCell;
+import com.pessdes.lyrics.ui.components.cells.SyncedLyricsCell;
 
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.MediaController;
@@ -254,7 +255,8 @@ public class LyricsActivity extends BaseFragment implements NotificationCenter.N
         @NonNull
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            LyricsCell lyricsCell = new LyricsCell(mContext);
+            log("creating view holder " + viewType);
+            SyncedLyricsCell lyricsCell = new SyncedLyricsCell(mContext);
             lyricsCell.setGravity(Gravity.CENTER);
             lyricsCell.setPadding(0, AndroidUtilities.dp(8), 0, AndroidUtilities.dp(8));
 
@@ -265,7 +267,8 @@ public class LyricsActivity extends BaseFragment implements NotificationCenter.N
 
         @Override
         public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-            LyricsCell lyricsCell = (LyricsCell) holder.itemView;
+            log("binding view holder " + position);
+            SyncedLyricsCell lyricsCell = (SyncedLyricsCell) holder.itemView;
             SyncedLyricsLine line = lyricsLines.get(position);
             if (line != null) {
                 lyricsCell.setText(line.text);
@@ -274,8 +277,9 @@ public class LyricsActivity extends BaseFragment implements NotificationCenter.N
 
         @Override
         public int getItemCount() {
-            log("items");
-            return lyricsLines != null ? lyricsLines.size() : 0;
+            var count = lyricsLines != null ? lyricsLines.size() : 0;
+            log(count + " lines");
+            return count;
         }
     }
 }
