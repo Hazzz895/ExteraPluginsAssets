b30a166dfcad2edab251b041b6d00d649b52b238
Hazzz895 <kornilovily4@gmail.com>
2025-10-15 21:11:59 +0400

    add annotations

--- lyrics/dex/src/main/java/com/pessdes/lyrics/components/lrclib/dto/Lyrics.java
+++ lyrics/dex/src/main/java/com/pessdes/lyrics/components/lrclib/dto/Lyrics.java
@@ -10,21 +10,16 @@ import java.util.List;
 
 public class Lyrics {
     public double duration;
-    public String plainLyrics;
-    public String plainSyncedLyrics;
-    @Nullable
-    public List<SyncedLyricsLine> syncedLyrics;
-
+    public @Nullable String plainLyrics;
+    public @Nullable String plainSyncedLyrics;
+    public @Nullable List<SyncedLyricsLine> syncedLyrics;
     public Lyrics(
             double duration,
-            String plainLyrics,
-            String plainSyncedLyrics
+            @Nullable String plainLyrics,
+            @Nullable String plainSyncedLyrics
     ) {
         this.duration = duration;
         this.plainLyrics = plainLyrics;
         this.plainSyncedLyrics = plainSyncedLyrics;
-        if (plainSyncedLyrics != null) {
-            this.syncedLyrics = LyricsController.getInstance().parseSyncedLyrics(plainSyncedLyrics);
-        }
     }
 }
